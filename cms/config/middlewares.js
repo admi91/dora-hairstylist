e5a4b6d14d4d6bc394320a58207696147d8480c7
gaetano.bisignano <gaetano.bisignano@arsenalia.com>
2026-01-02 00:36:10 +0100

    Fix DEFINITIVO: Aggiungi Cloudinary a Content Security Policy

--- cms/config/middlewares.js
+++ cms/config/middlewares.js
@@ -1,7 +1,31 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+            'https://market-assets.strapi.io',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
